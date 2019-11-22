a9fe6c14a1730f77a3afb92752ebb17f147e771e
Viktor <fredholm97@gmail.com>
2019-11-22 14:11:22 +0100

    created binary search for the highest and lowest index in the list

--- src/lab2/RangeBinarySearch.java
+++ src/lab2/RangeBinarySearch.java
@@ -5,11 +5,67 @@ import java.util.Comparator;
 public class RangeBinarySearch {
     // Returns the index of the first key in a[] that equals the search key, or -1 if no such key.
     // Complexity: O(log N), where N is the length of the array
-    public static <Key> int firstIndexOf(Key[] a, Key key, Comparator comparator)
+    public static <Key> int firstIndexOf(Key[] a, Key key, Comparator comparator){
+        int index = 0;
+        int lo = 0;
+        int high = a.length-1;
+        int mid;
+
+        while (true){
+            mid = (high-lo)/2;
+            int val = comparator.compare(a[mid], key);
+
+            if (val<0) {
+                lo = mid;
+            } else if (val > 0){
+                high = mid;
+            } else {
+                if (mid == 0){
+                    break;
+                }
+                //if the element before a[mid] is different from a[mid] break the loop and return index
+                if (comparator.compare(a[mid], a[mid - 1]) != 0) {
+                    break;
+                } else {
+                    high = mid;
+                }
+            }
+        }
+
+        return index;
+    }
 
     // Returns the index of the last key in a[] that equals the search key, or -1 if no such key.
     // Complexity: O(log N)
-    public static <Key> int lastIndexOf(Key[] a, Key key, Comparator comparator)
+    public static <Key> int lastIndexOf(Key[] a, Key key, Comparator comparator){
+        int index = -1;
+        int lo = 0;
+        int high = a.length-1;
+        int mid;
+
+        while (true){
+            mid = (high-lo)/2;
+            int val = comparator.compare(a[mid], key);
+
+            if (val<0) {
+                lo = mid;
+            } else if (val > 0){
+                high = mid;
+            } else {
+                if (mid == a.length-1){
+                    break;
+                }
+                //if the element before a[mid] is different from a[mid] break the loop and return index
+                if (comparator.compare(a[mid], a[mid + 1]) != 0) {
+                    break;
+                } else {
+                    lo = mid;
+                }
+            }
+        }
+        return index;
+    }
+
 
     //Corner cases
     //
