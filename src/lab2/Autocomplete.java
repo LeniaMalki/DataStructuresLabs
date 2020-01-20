2992324987af81533f789ddd946177ac2cc0e524
Johannes Mattsson <karlmattsson96@gmail.com>
2020-01-20 15:46:16 +0100

    Remove print

--- src/lab2/Autocomplete.java
+++ src/lab2/Autocomplete.java
@@ -28,7 +28,6 @@ public class Autocomplete {
         }
 
         int lastIndex  = RangeBinarySearch.lastIndexOf (this.terms, new Term(prefix, 0), Term.byPrefixOrder(prefix.length()));
-        System.out.println(firstIndex + "  " + lastIndex);
         Term[] terms = new Term[lastIndex - firstIndex + 1];
 
         for (int i = 0; i < terms.length; i++) {
